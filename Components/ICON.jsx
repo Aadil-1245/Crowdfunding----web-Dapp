788ef57b7129d5050f89d7bb7846713483fac9f3
Aadil-1245 <135581341+Aadil-1245@users.noreply.github.com>
2025-04-11 18:33:47 +0530

    Update ICON.jsx

--- Components/ICON.jsx
+++ Components/ICON.jsx
@@ -1,14 +1,27 @@
-import React from "react";
-
-const ICON = () => {
-  return (
-    <svg className="relative bg-opacity-75 backgroundMain" viewBox="0 0 1160 163">
-      <path
-        fill="currentColor"
-        d="M-164 13L-104 39.7C-44 66 76 120 196 141c316 162 436 152 556 119.7c676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C 76 162.5 -44 162.5 -104 162.5H-164V13Z"
-      />
-    </svg>
-  );
-};
-
-export default ICON;
+<svg
+  className={`w-8 ${color} text-teal-accent-400`}
+  viewBox="0 0 1400 200"
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+>
+  {/* Corrected path */}
+  <path
+    fill="currentColor"
+    d="M -164 13 
+       L -104 39.7 
+       C -44 66 76 120 196 141 
+       C 316 162 436 152 556 119.7 
+       C 676 88 796 34 916 13 
+       C 1036 -8 1156 2 1216 7.7 
+       L 1276 13 
+       V 162.5 
+       H 1216 
+       C 1156 162.5 1036 162.5 916 162.5 
+       C 796 162.5 676 162.5 556 162.5 
+       C 436 162.5 316 162.5 196 162.5 
+       C 76 162.5 -44 162.5 -104 162.5 
+       H -164 
+       V 13 
+       Z"
+  />
+</svg>
